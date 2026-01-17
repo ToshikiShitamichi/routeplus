9441277028b32ddeb1e68236d5b34f32e2f97b38
ToshikiShitamichi <t.shitamichi0123@gmail.com>
2026-01-17 12:12:48 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+import Index from "./pages/index"
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </Router>
+
+    </>
+  )
+}
+
+export default App

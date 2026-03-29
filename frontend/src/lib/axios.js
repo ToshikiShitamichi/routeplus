165b448316abc14c88482555114a9e30c78dc19b
ToshikiShitamichi <t.shitamichi0123@gmail.com>
2026-03-29 13:50:46 +0900

    baseURLを環境変数化

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
